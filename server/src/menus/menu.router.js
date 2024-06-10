e16861abbe05c7969196a7540df8e7ea8a72fa0f
Muhammad Fadhiil <fadhilrmdhn3010@gmail.com>
2024-06-10 14:36:32 +0700

    fix all

--- server/src/menus/menu.router.js
+++ server/src/menus/menu.router.js
@@ -4,6 +4,11 @@ const menuRouter = express.Router();
 
 menuRouter.get("/api/menus", menuController.getAllMenu);
 menuRouter.get("/api/menu/:id", menuController.getMenuById);
+
+menuRouter.get("/api/menuos", function(req, res) {
+  res.send("Hello World!");
+});
+
 menuRouter.put(
   "/api/menu/:id/availability",
   menuController.updateMenuAvailability
