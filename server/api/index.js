2b3cbf1444faa556efc83f7f7a3a359d6d3cad22
Muhammad Fadhiil <fadhilrmdhn3010@gmail.com>
2024-06-11 14:57:16 +0700

    feat: Vercel json for react

--- server/api/index.js
+++ server/api/index.js
@@ -26,4 +26,8 @@ app.get("/", function(req, res) {
   res.send("Hello World!");
 });
 
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
 module.exports = app;  // Exporting app as module
